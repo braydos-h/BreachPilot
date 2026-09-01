c9b358b8ce510ca7dc7bcc0ae74e9088951b0969
Brayden Habets <brayden.habets@icloud.com>
2026-09-01 14:27:59 +0930

    Delete demo directory

--- demo/src/routes/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="container">
-      <h1>BreachPilot Demo</h1>
-      <p>Minimal demo scaffold. This page is a placeholder.</p>
-    </main>
-  );
-}
